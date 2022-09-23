343daaf2f3d31e2e0d821798687eb87d377cc5a6
leawvc <43610417+leawvc@users.noreply.github.com>
2022-09-23 17:48:18 +0900

    Update Backjoon1541.java

--- Backjoon/Backjoon1541.java
+++ Backjoon/Backjoon1541.java
@@ -4,18 +4,20 @@ import java.io.InputStreamReader;
 import static java.lang.System.in;
 
 public class Backjoon1541 {
-    BufferedReader br =new BufferedReader(new InputStreamReader(in));
-    String [] num = br.readLine().split("-");
-    int sum = 0;
-        for (int i = 0; i<num.length; i++){
-        int k = 0;
-        String []num1 = num[i].split("\\+");
-        for (int j = 0; j < num1.length; j++){
-            k += Integer.parseInt(num1[j]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(in));
+        String num[] = br.readLine().split("-");
+        int sum = 0;
+        for (int i = 0; i < num.length; i++){
+            int j = 0;
+            String num1[] = num[i].split("\\+");
+            for (int k = 0; k < num1.length; k++){
+                j += Integer.getInteger(num1[k]);
+            }
+            if (i == 0){
+                sum+= j;
+            }else sum-=j;
         }
-        if (i==0){
-            sum += k;
-        }else sum -= k;
-    }
         out.println(sum);
+    }
 }
