ac2afa0a1f542542f204f06301b82a4a44683dc4
leawvc <43610417+leawvc@users.noreply.github.com>
2022-09-30 16:51:24 +0900

    Create [현대모비스][모의테스트] 구름 크기 측정하기.java

---
+++ groom/[현대모비스][모의테스트] 구름 크기 측정하기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int m = Integer.parseInt(input[1]);
+		System.out.print(n * m);
+	}
+}
