2ede5b6e987fb89d777d7fc1e17caec989b5f5f0
leawvc <43610417+leawvc@users.noreply.github.com>
2022-10-04 19:43:46 +0900

    Create 문자열 압축.java

---
+++ 프로그래머스/문자열 압축.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 1000;
+                
+        if(s.length() < 2) {
+            return s.length();
+        }
+        
+        for(int i = 1; i < s.length() / 2 + 2 ; i++) {    
+            String subStr = s.substring(0, i);
+            String output = "";
+            int count = 1;
+            int lastIndex = 0;
+            
+            for(int j = i ; j + i <= s.length(); j += i) {
+                String strToCompare = s.substring(j, j + i);
+                
+                if(subStr.equals(strToCompare)) {
+                    count++;
+                } else {
+                    if(count > 1) {
+                        output += count;
+                        count = 1;
+                    }
+                    output += subStr;
+                    subStr = strToCompare;
+                    
+                }
+                lastIndex = j + i;
+            }
+            
+            if(count > 1)
+                output += count;
+            output += subStr;
+            
+            if(lastIndex < s.length()) {
+            	output += s.substring(lastIndex);
+            }
+            
+            answer = Math.min(answer, output.length());
+        }
+        
+        return answer;
+    }
+}
