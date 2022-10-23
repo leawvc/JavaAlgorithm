315113c44f831b37a38c8ff1acf5eb735f666683
leawvc <43610417+leawvc@users.noreply.github.com>
2022-10-23 22:26:06 +0900

    Create 이진 탐색.java

---
+++ 탐색/이진 탐색.java
@@ -0,0 +1,42 @@
+// 이진 탐색 알고리즘
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static int binarySearch(int[] arr, int target, int start, int end){
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if (arr[mid] == target){
+                return mid;
+            }else if (arr[mid] > target){
+                end = mid - 1;
+            }
+            else end = mid + 1;
+        }
+        return  -1;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int x = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        int [] arr = new int[x];
+
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < x; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = binarySearch(arr, target, 0, x);
+        if (result == -1) {
+            System.out.println("원소가 존재하지 않습니다.");
+        }
+        else {
+            System.out.println(result + 1);
+        }
+
+    }
+}
