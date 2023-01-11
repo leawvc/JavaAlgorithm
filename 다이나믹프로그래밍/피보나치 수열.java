dd42e4bd09fa9e991cdf08d8dfc8e367099b15d1
leawvc <43610417+leawvc@users.noreply.github.com>
2023-01-11 20:45:05 +0900

    Create 피보나치 수열.java

---
+++ 다이나믹프로그래밍/피보나치 수열.java
@@ -0,0 +1,12 @@
+class Main {
+    public static int fibo(int x) {
+        if (x == 1 || x == 2) {
+            return 1;
+        }else {
+            return fibo(x - 1) + fibo(x - 2);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(fibo(6));
+    }
+}
