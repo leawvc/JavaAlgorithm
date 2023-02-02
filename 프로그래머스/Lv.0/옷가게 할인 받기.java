cef7efea7b7a2ac8174d01b24b39a3167c98b400
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-02 19:05:29 +0900

    Update 옷가게 할인 받기.java

--- 프로그래머스/Lv.0/옷가게 할인 받기.java
+++ 프로그래머스/Lv.0/옷가게 할인 받기.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int price) {
-        if(price > 500000) return (price * 80 / 100);
-        if(price > 300000) return (price * 80 / 100);
-        else return (price * 95 / 100);
+        if(price >= 500000) return (price * 80 / 100);
+        if(price >= 300000) return (price * 90 / 100);
+        if(price >= 100000) return (price * 95 / 100);
+        return price;
     }
 }
