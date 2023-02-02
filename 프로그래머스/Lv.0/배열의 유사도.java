edebc02f065870945765cfbce0d5ff369a9982b9
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-02 19:09:33 +0900

    Create 배열의 유사도.java

---
+++ 프로그래머스/Lv.0/배열의 유사도.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        int max = 0;
+        int min = 0;
+        if(s1.length >= s2.length){
+            for(int i = 0; i < s1.length; i++){
+                for(int j = 0; j < s2.length; j++){
+                    if(s1[i].equals(s2[j])){
+                        answer++;
+                    }
+                }
+            }
+        }else{
+            for(int i = 0; i < s2.length; i++){
+                for(int j = 0; j < s1.length; j++){
+                    if(s2[i].equals(s1[j])){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
