bb4a4a1bcefbb3b97442fbf9e6996ceb92be7705
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-03 21:47:33 +0900

    Create 개미 군단.java

---
+++ 프로그래머스/Lv.0/개미 군단.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        if(hp > 3){
+            answer += hp/5;
+            hp = hp % 5;
+        }if(hp > 2){
+            answer += hp/3;
+            hp = hp % 3;
+        }answer = answer + hp;
+        return answer;
+    }
+}
