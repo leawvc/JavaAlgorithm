4e80e1a95af250d5b73d1ec6a7fb60ae125275fc
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-03 22:05:01 +0900

    Update 암호 해독.java

--- 프로그래머스/Lv.0/암호 해독.java
+++ 프로그래머스/Lv.0/암호 해독.java
@@ -11,3 +11,17 @@ class Solution {
         return answer;
     }
 }
+
+
+==============================================================
+    
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer += cipher.substring(i - 1, i);
+        }
+        return answer;
+    }
+}
