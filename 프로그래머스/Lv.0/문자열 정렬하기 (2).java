519367a480d5b411419474e5fbf37b3a8b93b1a5
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-09 20:15:23 +0900

    Update 문자열 정렬하기 (2).java

--- 프로그래머스/Lv.0/문자열 정렬하기 (2).java
+++ 프로그래머스/Lv.0/문자열 정렬하기 (2).java
@@ -10,3 +10,13 @@ class Solution {
         return answer;
     }
 }
+
+============================================
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+    }
+}
