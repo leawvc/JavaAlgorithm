7dd1c8d5ceb0360c596617fe37481995d7975296
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-09 17:29:27 +0900

    Update 인덱스 바꾸기.java

--- 프로그래머스/Lv.0/인덱스 바꾸기.java
+++ 프로그래머스/Lv.0/인덱스 바꾸기.java
@@ -1,10 +1,11 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        char str = my_string.charAt(num1);
-        char str2 = my_string.charAt(num2);
-        String answer = my_string.replace(str, str2);
-        answer.replace(str2, str);
-        System.out.println(str +" " + str2);
-        return answer;
+        
+        char[] chars = my_string.toCharArray();
+        chars[num1] = my_string.charAt(num2);
+        chars[num2] = my_string.charAt(num1);
+        
+        my_string = String.valueOf(chars);
+        return my_string;
     }
-}
+} 
