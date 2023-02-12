53dbed5bb71fac4bfa5b0687b2367512b7b85a26
leawvc <43610417+leawvc@users.noreply.github.com>
2023-02-12 22:43:08 +0900

    Update 중복된 문자 제거.java

--- 프로그래머스/Lv.0/중복된 문자 제거.java
+++ 프로그래머스/Lv.0/중복된 문자 제거.java
@@ -2,10 +2,8 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         for(int i = 0; i < my_string.length(); i++){
-            answer += 
-            for(int j = 1; j < my_string.length(); j++){
-                if(my_string.charAt(i).equals(my_string.charAt(j))){
-                }
+            if(my_string.indexOf(my_string.charAt(i)) == i){
+                answer += my_string.charAt(i);
             }
         }
         return answer;
