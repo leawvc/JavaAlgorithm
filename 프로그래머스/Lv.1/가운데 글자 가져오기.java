91db3ed41749cae7ba370180e442c3976bc67467
leawvc <43610417+leawvc@users.noreply.github.com>
2023-03-19 23:24:22 +0900

    Create 가운데 글자 가져오기.java

---
+++ 프로그래머스/Lv.1/가운데 글자 가져오기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length() / 2;
+        answer = s.length() % 2 == 0 ? s.substring(len - 1, len + 1) : s.substring(len, len + 1);
+        return answer;
+    }
+}
