37f90f183f2a8be5c04a96d2d8cd49555a4cfd51
leawvc <43610417+leawvc@users.noreply.github.com>
2023-03-30 00:07:14 +0900

    Update 숫자 문자열과 영단어.java

--- 프로그래머스/Lv.1/숫자 문자열과 영단어.java
+++ 프로그래머스/Lv.1/숫자 문자열과 영단어.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String s) {
         String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         for(int i = 0; i < 10; i++){
-            s = s.replace(word[i], Integer.toString(i));
+            s = s.replaceAll(word[i], Integer.toString(i));
         }
         return Integer.parseInt(s);
     }
